8fa6962cdbae75fadfa825ad9951a992754fc0fd
Piotr Tylenda <tylenda.piotr@gmail.com>
2015-11-19 02:09:27 +0100

    tornado coroutines

--- tornado_getresponse_app/app_queue.py
+++ tornado_getresponse_app/app_queue.py
@@ -17,13 +17,13 @@ def merge_dicts(x, y):
 class GetResponseConnector(object):
     token = ""  # Some dead test account
 
-    def get(self, path,):
+    def get(self, path, extra_headers):
         auth_headers = {'X-Auth-Token': 'api-key ' + self.token}
-        return requests.get(self._url(path), headers=auth_headers)
+        return requests.get(self._url(path), headers=merge_dicts(auth_headers, extra_headers))
 
-    def head(self, path):
+    def head(self, path, extra_headers):
         auth_headers = {'X-Auth-Token': 'api-key ' + self.token}
-        return requests.head(self._url(path), headers=auth_headers)
+        return requests.head(self._url(path), headers=merge_dicts(auth_headers, extra_headers))
 
     @staticmethod
     def _url(path):
@@ -82,31 +82,70 @@ class ListsResource(BaseResource):
 
 class StreamingHandler(tornado.web.RequestHandler):
 
-    client = tornado.httpclient.AsyncHTTPClient(max_clients=5)
-    connector_async = GetResponseConnectorAsync()
+    q = tornado.queues.Queue(maxsize=0)
+    sem = tornado.locks.BoundedSemaphore(value=10)
 
     @tornado.web.asynchronous
     @tornado.gen.coroutine
     def get(self):
-        connector = GetResponseConnector()
-        resp = connector.head("/contacts?perPage={0}".format(1000))
-        count = int(resp.headers['TotalPages']);
 
-        print(count)
 
-        yield [self.flush_page(page) for page in range(1, count + 1)]
+        # self.write(str(resp.headers))
+        # self.flush()
+
+
+
+
+
+        #resp = yield [self.flush_page(page) for page in range(1, count + 1)]
+
+
+        tornado.ioloop.IOLoop.current().spawn_callback(self.consumer)
+        yield self.producer()     # Wait for producer to put all tasks.
+        yield self.q.join()       # Wait for consumer to finish all tasks.
+
+
+
+
+        #for page, response in resp.items():
+        #    self.write(response.body)
+
+        # yield list(map(client.fetch, data))
 
         self.finish()
 
 
     @tornado.gen.coroutine
-    def flush_page(self, page):
-        print("processing", page)
-        req = self.connector_async.get(path="/contacts?perPage={0}&page={1}".format(1000, page))
-        resp = yield self.client.fetch(req)
-        print("flushing", page)
-        self.write(resp.body)
+    def producer(self):
+        client = tornado.httpclient.AsyncHTTPClient(max_clients=10)
+        connector_async = GetResponseConnectorAsync()
+        connector = GetResponseConnector()
+        resp = connector.head("/contacts", {'perPage': 1000})
+        count = int(resp.headers['TotalPages']);
+
+        print(count)
+
+        for page in range(1, count + 1):
+            job = client.fetch(connector_async.get(path="/contacts?perPage={0}&page={1}".format(1000, page)))
+            yield self.q.put(job)
+
+    @tornado.gen.coroutine
+    def consumer(self):
+        while True:
+            job = yield self.q.get()
+            try:
+                self.sem.acquire()
+                yield self.process(job)
+            finally:
+                self.q.task_done()
+
+    @tornado.gen.coroutine
+    def process(self, job):
+        print("processing!")
+        response = yield job
+        self.write(response.body)
         self.flush()
+
 
 app = tornado.web.Application([
     (r"/lists", StreamingHandler)
