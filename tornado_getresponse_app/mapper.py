5dd15f642683d4ffd4c5e555f4d6c9ab18bb1653
Piotr Tylenda <tylenda.piotr@gmail.com>
2015-11-30 00:47:58 +0100

    Changes in mappers

--- tornado_getresponse_app/mapper.py
+++ tornado_getresponse_app/mapper.py
@@ -5,15 +5,20 @@ from .domain import Member
 
 def json_newsletter_to_campaign(data):
     if 'newsletterId' in data:
-        return Campaign(data['newsletterId'], data['name'], data['subject'])
+        return Campaign(esp_id=data['newsletterId'],
+                        name=data['name'],
+                        subject=data['subject'])
     return data
 
 
 def json_campaign_to_list(data):
-    return List(data['campaignId'], data['name'])
+    return List(esp_id=data['campaignId'],
+                name=data['name'])
 
 
 def json_contact_to_member(data):
     if 'contactId' in data:
-        return Member(data['contactId'], data['name'], data['email'])
+        return Member(esp_id=data['contactId'],
+                      name=data['name'],
+                      email=data['email'])
     return data
